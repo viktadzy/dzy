54483adb63b7f44e60a804027e0e8d91bd3040e3
VIKTA DZY <viktamuhumuza@gmail.com>
2022-06-01 14:52:17 +0300

    first project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,12 @@ fun main(args: Array<String>) {
     val sum=x+y
 
     println("sum is $sum")
-    if (sum>13)
-        println("Wrong")
-    else
-        println("almost correct")
+     val instruments = listOf("trumpets","drums","pianos","violin")// immutable lists
+    println(instruments)
+    val mylist = mutableListOf("trumpets","drums","pianos","violin")//mutable lists
+
+    println(mylist)
+
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.

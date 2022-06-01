70f1ccbe361cb45792b250db694318b0b03359aa
VIKTA DZY <viktamuhumuza@gmail.com>
2022-06-01 14:31:39 +0300

    first project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val x=5
+    val y=8
+    val sum=x+y
+
+    println("sum is $sum")
+    if (sum>13)
+        println("Wrong")
+    else
+        println("almost correct")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+
+}
